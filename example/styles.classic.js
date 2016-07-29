0ce921e6775ca28d97d3fae0cb2a72bdf073c88c
Johann <johannhospice.dev@gmail.com>
2016-07-29 14:35:09 +0200

    Add a classic CSS stylesheet in js

--- example/styles.classic.js
+++ example/styles.classic.js
@@ -1,3 +1,29 @@
-/**
- * Created by Johann on 29/07/2016.
- */
+var noir = '#000',
+    pow2 = val => Math.pow(2, val);
+
+module.exports = {
+    '@font-face': {
+        src: 'url(../font.ttf)'
+    },
+    body: {
+        margin: 0
+    },
+    '#link': {
+        'background-color': noir,
+        height: pow2(2)
+    },
+    '.nested': {
+        color: '#333',
+    },
+    a: {
+        color: 'red'
+    },
+    'a:hover': {
+        opacity: 'transparent'
+    },
+    'a::after': {
+        content: "''",
+        position: 'absolute',
+        top: 0
+    }
+}
