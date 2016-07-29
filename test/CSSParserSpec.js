c4bbaf09224ba4f84fedd95226dce4496bf7079b
Johann <johannhospice.dev@gmail.com>
2016-07-29 11:14:41 +0200

    Adding tests for declaration, selector and rule

---
+++ test/CSSParserSpec.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var vars = require('./vars'),
+	assert = require('assert'),
+	Rule = require('../lib/class/Rule'),
+	Selector = require('../lib/class/Selector'),
+	Declaration = require('../lib/class/Declaration');
+
+describe('CSSparser', function(){
+	describe('Selector', function(){
+		let selector = new Selector('.nav');
+		it('should have a good minified toString', function (done) {
+			assert.equal(selector.toString(), '.nav');
+			done();
+		});
+	});
+
+	describe('Declaration', function(){
+		let declaration = new Declaration('background', '#000')
+		it('should have a good minified toString', function (done) {
+			assert.equal(declaration.toString(), 'background:#000');
+			done();
+		});
+		it('should have a good beauty toString', function (done) {
+			assert.equal(declaration.toStringBeauty(), 'background: #000');
+			done();
+		});
+	})
+	
+	describe('Rule', function(){
+		let rule = new Rule('.nav', {
+			'background-color': 'red',
+			position: 'absolute'
+		});
+		it('should have a good minified toString', function (done) {
+			assert.equal(rule.toString(), ".nav{background-color:red;position:absolute}");
+			done();
+		});
+		it('should have a good beauty toString', function (done) {
+			assert.equal(rule.toStringBeauty(), ".nav {\n\tbackground-color: red;\n\tposition: absolute\n}");
+			done();
+		});
+	})
+})
