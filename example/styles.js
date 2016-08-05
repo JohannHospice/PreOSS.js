980376e852d466b1e27c93edf00b5921a44307ce
Johann <johannhospice.dev@gmail.com>
2016-08-05 10:16:53 -0400

    Add for example css transpile file and remove useless

--- example/styles.js
+++ example/styles.js
@@ -1,26 +1,39 @@
-var noir = '#000';
+var colors = {
+        grey:[
+            '#212121',
+            '#424242',
+            '#717171',
+        ]
+    },
+    pow2 = val => Math.pow(2, val);
 
 module.exports = {
-	'@font-face': {
-		src: 'url(../font.ttf)'
-	},
-	body: {
-		margin: 0
-	},
-	'#link': {
-		'background-color': noir,
-		height: 8+4
-	},
-	'.nested': {
-		color: '#333',
-		a: {
-			color: 'red'
-		},
-		'a:hover': {
-			opacity: 'transparent'
-		}
-	},
-	'a::after': {
-		content:''
-	}
+    '@font-face': {
+
+        src: "url('../font/font.ttf')"
+    },
+    body: {
+        margin: 0,
+        padding: 15+'px'
+    },
+    '#link': {
+        'background-color': colors.grey[1],
+        height: `${pow2(8)}px`,
+        width: 'auto',
+    },
+    '.nested': {
+        color: '#333',
+    },
+    a: {
+        color: 'red'
+    },
+    'a:hover': {
+        'text-align': 'center',
+        opacity: '0'
+    },
+    'a::after': {
+        content: "''",
+        position: 'absolute',
+        top: 0
+    }
 }
