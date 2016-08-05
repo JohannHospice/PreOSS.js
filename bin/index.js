68902afcc9665d7d54fc2afc9535427bb4a1b1e1
Johann <johannhospice.dev@gmail.com>
2016-08-05 10:21:06 -0400

    Complete preprocessing

--- bin/index.js
+++ bin/index.js
@@ -8,13 +8,12 @@ let preprocessor = require('../lib/preprocessor'),
     defaultConfig = require('../lib/constants/defaultConfig'),
     file = require('../lib/file');
 
-command.parse(process.argv, (err, options, files)=> {
+command.parse(process.argv, function (err, options, others) {
     if (err)
         throw new Error(err);
 
-    files = files.map(file => __dirname + file);
-
-    let config = defaultConfig;
+    let config = defaultConfig,
+        paths = others.map(file => `./${file}`);
 
     options.forEach(function (arg) {
         switch (arg) {
@@ -42,19 +41,32 @@ command.parse(process.argv, (err, options, files)=> {
             case '-h':
                 printUsage();
                 break;
+            default:
+                printUnknowCommand();
         }
     });
 
-    if (files.length == 2)
-        preprocessor.exec(files[0], files[1], config);
+    if (paths.length != 2)
+        printNeedArgs();
 
+    preprocessor.exec(paths[0], paths[1], config);
+});
+
+function printNeedArgs() {
+    console.error('Need 2 arguments\nUse: press --help');
+    process.exit(1);
+}
+
+function printUnknowCommand() {
     console.error('Unknow command\nUse: press --help');
     process.exit(1);
-});
+}
+
 function printVersion() {
     console.log(JSON.parse(file.readSync(__dirname + '/../package.json')).version)
     process.exit(1);
 }
+
 function printUsage() {
     console.log([
         "Usage: press <options> <source_file> <destination_file>\n",
